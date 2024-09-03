f5153905034fabd2de76f09f607649e7df0778b8
Dhruv Khatter <112469222+LearnWithDhruv@users.noreply.github.com>
2024-09-03 12:21:25 +0530

    Update visualization_utils.py

--- src/utils/visualization_utils.py
+++ src/utils/visualization_utils.py
@@ -1,11 +1,7 @@
-# src/utils/visualization_utils.py
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 def plot_correlation_matrix(corr_matrix):
-    """
-    Plot the correlation matrix as a heatmap.
-    """
     plt.figure(figsize=(10, 8))
     sns.heatmap(corr_matrix, annot=True, fmt=".2f")
     plt.title('Correlation Matrix')
